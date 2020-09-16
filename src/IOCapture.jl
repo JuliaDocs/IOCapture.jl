9d6829534fb09f9b58e602306b725ed4fde19c26
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-09-16 18:27:08 +1200

    Remove unnecessary empty line

--- src/IOCapture.jl
+++ src/IOCapture.jl
@@ -59,7 +59,6 @@ function iocapture(f)
             # InterruptException should never happen during normal doc-testing
             # and not being able to abort the doc-build is annoying (#687).
             isa(err, InterruptException) && rethrow(err)
-
             err, false, catch_backtrace()
         finally
             # Force at least a single write to `pipe`, otherwise `readavailable` blocks.
