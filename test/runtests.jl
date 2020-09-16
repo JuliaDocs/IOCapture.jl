a760de953582b2dfa7f442edb4d818283f4a0def
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-09-16 18:27:32 +1200

    Add depwarn test from Documenter's test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using IOCapture
 using Test
 
+# hasfield was added in Julia 1.2. This definition borrowed from Compat.jl (MIT)
+# Note: this can not be inside the testset
+(VERSION < v"1.2.0-DEV.272") && (hasfield(::Type{T}, name::Symbol) where T = Base.fieldindex(T, name, false) > 0)
+
 # Callable object for testing
 struct Foo
     x
@@ -77,4 +81,32 @@ end
     @test !c.error
     @test c.output == "callable test\n"
     @test c.value === nothing
+
+    # Colors get discarded
+    c = iocapture() do
+        printstyled("foo", color=:red)
+    end
+    @test !c.error
+    @test c.output == "foo"
+    @test c.value === nothing
+
+    # This test checks that deprecation warnings are captured correctly
+    c = iocapture() do
+        println("println")
+        @info "@info"
+        f() = (Base.depwarn("depwarn", :f); nothing)
+        f()
+    end
+    @test !c.error
+    @test c.value === nothing
+    # The output is dependent on whether the user is running tests with deprecation
+    # warnings enabled or not. To figure out whether that is the case or not, we can
+    # look at the .depwarn field of the undocumented Base.JLOptions object.
+    @test isdefined(Base, :JLOptions)
+    @test hasfield(Base.JLOptions, :depwarn)
+    if Base.JLOptions().depwarn == 0 # --depwarn=no, default on Julia >= 1.5
+        @test c.output == "println\n[ Info: @info\n"
+    else # --depwarn=yes
+        @test startswith(c.output, "println\n[ Info: @info\n┌ Warning: depwarn\n")
+    end
 end
